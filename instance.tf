f46213599590a49ea164c465a1aac51c02525cf1
GowthamMuruganandhan25 <60310597+GowthamMuruganandhan25@users.noreply.github.com>
2021-02-23 19:47:27 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,164 @@
+provider "google" {
+ credentials = file("SAKEY.json")
+ project     = "gcp-training-01-303001"
+ region = "us-east1"
+}
+
+resource "google_compute_health_check" "autohealing" {
+  name                = "autohealing-health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10 # 50 seconds
+
+  http_health_check {
+    request_path = "/healthz"
+    port         = "8080"
+  }
+}
+
+resource "google_compute_instance_template" "gen1" {
+  name = "gowtham-mig-tmp1"
+  machine_type            = "n1-standard-1"
+  metadata_startup_script = file("startup.sh")
+  region                  = "us-east1"
+  tags = [ "http-server","http","https","allow-iap-ssh","allow-http" ]
+
+  scheduling {
+    automatic_restart = false
+    preemptible       = false
+  }
+
+  disk {
+    source_image = "debian-cloud/debian-9"
+    disk_type = "pd-ssd"
+    disk_size_gb = 10
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network    = google_compute_network.pokenav.self_link
+    subnetwork = google_compute_subnetwork.hoenn.self_link
+    access_config {
+      
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_instance_template" "gen2" {
+
+  name = "gowtham-mig-tmp2"
+  machine_type            = "n1-standard-1"
+  metadata_startup_script = file("startup.sh")
+  region                  = "europe-west1"
+  tags = [ "http-server","http","allow-incoming","allow-iap-ssh","allow-http"]
+
+  scheduling {
+    automatic_restart = false
+    preemptible       = false
+  }
+
+  disk {
+    source_image = "debian-cloud/debian-9"
+    disk_type = "pd-ssd"
+    disk_size_gb = 10
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network    = google_compute_network.pokenav.self_link
+    subnetwork = google_compute_subnetwork.kanto.self_link
+     access_config {
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+resource "google_compute_region_instance_group_manager" "mig_group1" {
+  name               = "gowtham-mig-g1"
+  region             = "us-east1"
+  base_instance_name = "gowtham-mig"
+  target_size        = 2
+
+  version {
+    instance_template = google_compute_instance_template.gen1.self_link
+    name              = "gowtham-gen1"
+  }
+  
+  named_port {
+    name = "http"
+    port = 80
+  }
+  
+  named_port {
+    name = "https"
+    port = 443
+  }
+}
+
+
+resource "google_compute_region_instance_group_manager" "mig_group2" {
+  name               = "gowtham-mig-g2"
+  region             = "europe-west1"
+  base_instance_name = "gowtham-mig"
+  target_size        = 2
+
+  version {
+    instance_template = google_compute_instance_template.gen2.self_link
+    name              = "gowtham-gen2"
+  }
+  
+  named_port {
+    name = "http"
+    port = 80
+  }
+  
+  named_port {
+    name = "https"
+    port = 443
+  }
+}
+
+resource "google_compute_region_autoscaler" "evo1" {
+  name   = "hoenn-region-autoscaler"
+  region = "us-east1"
+  target = google_compute_region_instance_group_manager.mig_group1.id
+
+  autoscaling_policy {
+    max_replicas    = 3
+    min_replicas    = 1
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.7
+    }
+  }
+}
+
+
+resource "google_compute_region_autoscaler" "evo2" {
+  name   = "kanto-region-autoscaler"
+  region = "europe-west1"
+  target = google_compute_region_instance_group_manager.mig_group2.id
+
+  autoscaling_policy {
+    max_replicas    = 3
+    min_replicas    = 1
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.7
+    }
+  }
+}
